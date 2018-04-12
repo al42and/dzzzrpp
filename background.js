59210c2eef2b550c29a391a336c320baa9a652cf
Andrey Alekseenko <al42and@gmail.com>
2018-04-12 17:20:16 +0300

    Minor improvements

---
+++ background.js
@@ -0,0 +1,18 @@
+// Use example from Chromium to control when extension button is clickable
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { hostEquals: 'classic.dzzzr.ru' },
+                }),
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { hostEquals: 'online.dzzzr.ru' },
+                })
+            ],
+            // And shows the extension's page action.
+            actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }]);
+    });
+});
--- popup.js
+++ popup.js
@@ -8,7 +8,6 @@ function setFieldColorCookie(ready) {
 
 function setCookie() {
     const cookie = document.getElementById('cookie').value;
-    console.log("setCookie..." + cookie);
     chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
         let tab = arrayOfTabs[0];
         const tabUrl = tab.url;
@@ -23,16 +22,13 @@ function getCookie() {
     chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
         let tab = arrayOfTabs[0];
         const tabUrl = tab.url;
-        console.log("url=" + tabUrl);
         chrome.cookies.get({url: tabUrl, name: 'dozorSiteSession'},
             function (cookie) {
                 if (cookie) {
-                    console.log("cookie=" + cookie.value);
                     document.getElementById('cookie').value = cookie.value;
                     setFieldColorCookie(true);
                 }
                 else {
-                    console.log("no cookie found");
                     document.getElementById('cookie').value = '<none>';
                     setFieldColorCookie(false);
                 }
@@ -45,9 +41,13 @@ function onLoad() {
     document.getElementById('cookie').oninput = function() {
         setFieldColorCookie(false);
     };
+    document.getElementById('cookie').onclick = function() {
+        this.select();
+    };
     document.getElementById('cookie_form').onsubmit = function() {
         setCookie();
     };
+    //chrome.cookies.onChanged.addListener(getCookie);
     getCookie();
 }
 
